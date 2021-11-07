e147062231cc2f37637a4287f94b0f2327095e33
Lydie Janovsky <lydiejanovsky@hotmail.fr>
2021-11-07 15:03:13 +0100

    Création de la fonction Javascript permettant de récupérer la liste des produits depuis l'API. Et remplacement de la liste statique de produits préalablement crée par celle récupérée au niveau de l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,41 +1,26 @@
-const products = [
-    {
-    img: "../../back/images/kanap04.jpeg",
-    name:"King Canap",
-    price: 1999,
-    description: "Super Canap"
-    },
-    {
-    img: "../../back/images/kanap03.jpeg",
-    name:"Queen Canap",
-    price: 2999,
-    description: "Super Canap"
-    },
-    {
-    img: "../../back/images/kanap02.jpeg",
-    name:"Wow Canap",
-    price: 13999,
-    description: "Super Canap"
-    },
-    {
-    img: "../../back/images/kanap01.jpeg",
-    name:"Mega Canap",
-    price: 999,
-    description: "Super Canap"
-    },
-]
+fetch('http://localhost:3000/api/products')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) { 
+        console.log(data.length);
+        products(data);
+    })
+    .catch(function(error) {
+        console.log("Error");
+    });
 
+function products(product) {
+  for(let i=0; i<product.length; i++) {
+    console.log(product[i]);
 
-
-products.forEach(product => {
-    document.querySelector('#items').innerHTML += `<a href="./product.html?id=42">
-    <article>
-      <img src=${product.img} alt="Lorem ipsum dolor sit amet, Kanap name1"/>
-      <h3 class="productName">${product.name}</h3>
-      <p class="productDescription">${product.description}</p>
-      <p>${product.price} €</p>
-    </article>
-  </a>`;
-});
-
-
+    document.querySelector('#items').innerHTML += `<a href="./product.html?id=${product[i]._id}">
+        <article>
+            <img src=${product[i].imageUrl} alt="Lorem ipsum dolor sit amet, Kanap name1">
+            <h3 class="productName">${product[i].name}</h3>
+            <p class="price" >${product[i].price}</p>
+            <p class="productDescription">${product[i].description}</p>
+        </article>
+        </a>`;
+  }  
+}
